3e2a4c8be9acb0d6a1d0ba22244d97fd0ad8fd30
NageshKanneganti <nk726@njit.edu>
2024-02-16 11:04:47 -0600

    Reverted calculator test file to basic tests

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,24 +1,24 @@
 '''Test File: __init__.py'''
 # Disable specific pylint warnings that are not relevant for this file.
 # pylint: disable=unnecessary-dunder-call, invalid-name
-from decimal import Decimal
 import pytest
 from calculator import Calculator
 
-@pytest.mark.parametrize("calculator_op, a, b, expected", [
-    (Calculator.add, Decimal('2'), Decimal('2'), Decimal('4')),
-    (Calculator.subtract, Decimal('2'), Decimal('2'), Decimal('0')),
-    (Calculator.multiply, Decimal('2'), Decimal('2'), Decimal('4')),
-    (Calculator.divide, Decimal('2'), Decimal('2'), Decimal('1')),
-    (Calculator.add, Decimal('1.0'), Decimal('2.0'), Decimal('3.0')),
-    (Calculator.subtract, Decimal('3.0'), Decimal('2.0'), Decimal('1.0')),
-    (Calculator.multiply, Decimal('2.0'), Decimal('3.0'), Decimal('6.0')),
-    (Calculator.divide, Decimal('6.0'), Decimal('2.0'), Decimal('3.0')),
-])
+def test_addition():
+    '''Test that calculator add function works '''    
+    assert Calculator.add(2,2) == 4
 
-def test_calculator_operations(calculator_op, a, b, expected):
-    '''Test Calculator class _perform_calculations method'''
-    assert calculator_op(a, b) == expected, f"Failed {calculator_op.__name__} operation with {a} and {b}"
+def test_subtraction():
+    '''Test that calculator subtract function works '''    
+    assert Calculator.subtract(2,2) == 0
+
+def test_multiply():
+    '''Test that calculator multiply function works '''    
+    assert Calculator.multiply(2,2) == 4
+
+def test_divide():
+    '''Test that calculator divide function works '''    
+    assert Calculator.divide(2,2) == 1
 
 def test_calculator_divide_by_zero():
     '''Test calculator divide by zero exception'''
